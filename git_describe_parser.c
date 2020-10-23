f369d06c60357bf58ba56509e5b2dd134da6b0b9
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-10-22 22:46:49 -0300

    Implements didatic main function to see the module working

--- git_describe_parser.c
+++ git_describe_parser.c
@@ -20,9 +20,26 @@
 #ifdef _TEST_MODULE
 int main(void)
 {
-    printf("Raw data:\n");
-    printf(_GIT_DESCRIPTION_STR "\n");
-    printf(_GIT_COMMIT_HASH_STR "\n");
+    git_description_t my_description = git_describe_parse();
+
+    printf("Commit hash: %s\n", my_description.commit_hash_short);
+    printf("Raw description: %s\n", my_description.raw_description);
+    printf("Version numbers: ");
+    for (int counter = 0; counter < my_description.used_version_numbers; counter++) {
+        printf("%i.", my_description.version_numbers[counter]);
+    }
+    printf("\b \n");
+    printf("There are %i commits after the last tag\n", my_description.commits_after_tag);
+    if (my_description.release_candidate_number < 0) {
+        printf("It's a release!\n");
+    } else {
+        printf("It's just the release candidate #%i\n", my_description.release_candidate_number);
+    }
+    if (my_description.is_dirty) {
+        printf("There are uncommited changes\n");
+    } else {
+        printf("Working tree clean\n");
+    }
 
     return 0;
 }
@@ -38,7 +55,7 @@ git_description_t git_describe_parse(void)
         .commits_after_tag          = INVALID_INTEGER_RESULT,
         .used_version_numbers       = INVALID_INTEGER_RESULT,
         .release_candidate_number   = INVALID_INTEGER_RESULT,
-        .is_dirty                   = false,
+        .is_dirty                   = true,
     };
     for (int counter = 0; counter < TAG_MAX_VERSION_NUMBERS; counter++) {
         result.version_numbers[counter] = INVALID_INTEGER_RESULT;
