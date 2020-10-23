9fc874c1c99ceeb3b3640164c411699450a66a7b
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-10-22 22:46:14 -0300

    Documents components of the struct

--- git_describe_parser.h
+++ git_describe_parser.h
@@ -9,12 +9,18 @@
 /* Public types ------------------------------------------------------------- */
 typedef struct
 {
+    // Result of 'git rev-parse HEAD'
     char * commit_hash_short;
+    // Result of 'git describe --dirty=-dirty'
     char * raw_description;
+    // First numbers of tag, separated by any separator, except '-'
     int version_numbers[TAG_MAX_VERSION_NUMBERS];
-    int commits_after_tag;
     int used_version_numbers;
+    // Number of commits after the described tag
+    int commits_after_tag;
+    // Number after symbol '-RC', returns negative if does not exist '-RC'
     int release_candidate_number;
+    // 'false' if working tree clean and 'true' if there are uncommited changes
     bool is_dirty;
 } git_description_t;
 
